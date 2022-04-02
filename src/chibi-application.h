83c274bae04f0547f267bbcf708763bfae609c70
Alexandros Theodotou <alex@zrythm.org>
2022-04-02 17:31:30 +0100

    use SPDX identifiers (fixes #2)

--- src/chibi-application.h
+++ src/chibi-application.h
@@ -1,22 +1,7 @@
-/*
- * Chibi - Carla's mini-host plugin loader
+// SPDX-License-Identifier: AGPL-3.0-or-later
+/* Chibi - Carla's mini-host plugin loader.
  *
  * Copyright (C) 2022 Alexandros Theodotou <alex at zrythm dot org>
- *
- * This file is part of Chibi
- *
- * Chibi is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Chibi is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with Chibi.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef __CHIBI_APPLICATION_H__
